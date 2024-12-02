f8bed809e8d6f3725d23a925bc98567a2b682526
Hasan Ali <hasan15.06.96@gmail.com>
2024-12-02 23:54:04 +0000

    design touchups

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
     <div className="relative h-full w-full overflow-clip">
       <Image
         src="/starry-night.jpg"
-        className="absolute z-0 h-full w-full min-w-[1000px] opacity-10"
+        className="absolute z-0 h-full w-full min-w-[1000px] opacity-[0.08]"
         width={1000}
         height={1000}
         alt="-"
@@ -29,20 +29,20 @@ export default function Home() {
               target="_blank"
               href="http://www.linkedin.com/in/hasan-ali-software-developer"
             >
-              <Linkedin className="h-6 w-6 rounded-sm border border-blue p-1 hover:bg-white/10 md:h-8 md:w-8" />
+              <Linkedin className="h-8 w-8 rounded-sm border-blue p-1 hover:bg-white/10 md:h-8 md:w-8" />
             </a>
             <a
               target="_blank"
               href="https://github.com/hasan-ali101?tab=overview&from=2024-12-01&to=2024-12-02"
             >
-              <Github className="h-6 w-6 border border-blue p-1 hover:bg-white/10 md:h-8 md:w-8" />
+              <Github className="h-8 w-8 border-blue p-1 hover:bg-white/10 md:h-8 md:w-8" />
             </a>
           </div>
           <div className="flex items-center gap-2 text-xs font-semibold sm:text-sm md:gap-3 md:text-base">
-            <button className="border border-blue p-1 hover:bg-white/10">
+            <button className="rounded-xl border border-blue p-1 px-3 hover:bg-white/10">
               Contact
             </button>
-            <button className="border border-blue p-1 px-2 hover:bg-white/10">
+            <button className="rounded-xl border border-blue p-1 px-3 hover:bg-white/10">
               CV
             </button>
           </div>
@@ -50,16 +50,14 @@ export default function Home() {
 
         <div
           id="haes"
-          className={`${akira.className} items flex w-full items-center justify-between border-y border-y-beige py-1 text-center text-2xl tracking-wider sm:py-4 sm:text-4xl`}
+          className={`${akira.className} items flex w-full items-center justify-between border-y border-y-beige py-1 text-center text-3xl sm:py-4 sm:text-4xl sm:tracking-wider`}
         >
           <div className="mx-3 flex flex-1 items-center gap-1">
-            <Plus className="mx-2 h-5 w-5" />
-            <div className="flex-1 border-t-[0.5px] border-beige" />
+            <div className="ml-2 flex-1 border-t-[0.5px] border-beige md:ml-12" />
           </div>{" "}
-          <h1 className="mx-2">STUDIO HAES</h1>
+          <h1 className="md:mx-2">STUDIO HAES</h1>
           <div className="mx-3 flex flex-1 items-center gap-1">
-            <div className="flex-1 border-t-[0.5px] border-beige" />
-            <Plus className="mx-2 h-5 w-5" />
+            <div className="mr-2 flex-1 border-t-[0.5px] border-beige md:mr-12" />
           </div>
         </div>
 
